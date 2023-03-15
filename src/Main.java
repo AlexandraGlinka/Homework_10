a6e2054c46f8103d2d79199d2073166b4bc5cd80
glinka <alexandraglinka@yandex.ru>
2023-03-15 21:59:23 +0300

    Task_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1(){
+        System.out.println("Задача 1");
+//        Напишите четыре строки:
+//        С именем firstName — для хранения имени.
+//                С именем middleName — для хранения отчества.
+//                С именем lastName — для хранения фамилии.
+//                С именем fullName — для хранения Ф. И. О. сотрудника в формате "Фамилия Имя Отчество".
+//                Выведите в консоль фразу: «ФИО сотрудника — …».
+//        В качестве данных для задачи используйте "Ivanov Ivan Ivanovich".
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
